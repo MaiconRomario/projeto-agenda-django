402b077755720b653e32636fd24dfb2d7ee1a909
MaiconRomario <maiconromario389@hotmail.com>
2025-08-27 22:00:37 -0300

    creating model Category and ForeignKey

--- contact/models.py
+++ contact/models.py
@@ -2,7 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 
-# Create your models here.
+# Create your models here
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -11,6 +17,10 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
+    
